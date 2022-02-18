005c3baf2ef502943a46cba65cb806959639b096
Student <ramkashyap2050@gmail.com>
2022-02-17 19:11:11 -0500

    first Commit

---
+++ execise01.py
@@ -0,0 +1,12 @@
+# Create the variables with int values
+
+x = int(input(f"Enter X"))
+y = int(input(f"Enter Y"))
+
+# Add two variables and store it in a third value
+
+z = x+y
+
+# Print out all three variables
+
+print(f"{x, y, z}")
---
+++ main.py
@@ -0,0 +1,21 @@
+# This is all data types
+
+5  # int
+5.5  # float
+'5.5'  # String
+
+print(len('hello'))  # Length of a string
+
+# Math Operators
+c = 5 + 5
+a = 5 / 5
+b = 5 * 5
+d = 5 - 5
+
+print(a, b, c, d)
+
+# Read a variable
+
+myvar = int(input("Enter a number"))
+
+print(myvar + 2)
